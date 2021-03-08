74a72ea2953c47eb5166921b705ebfa0794c5b84
Nikki Tebaldi <ntebaldi@umass.edu>
2021-03-08 16:52:52 -0500

    Removed directory info

--- app/data/config.py
+++ app/data/config.py
@@ -1,6 +1,6 @@
 extract_config = {
-    "no_cores" : 5,
-    "input_dir" : "/home/nikki/Documents/confluence/workspace/extract/io/extract_input_5",
-    "output_dir" : "/home/nikki/Documents/confluence/workspace/extract/io/extract_output_5",
-    "logging_dir" : "/home/nikki/Documents/confluence/workspace/extract/extract/logs"
+    "no_cores" : 1,
+    "input_dir" : "",
+    "output_dir" : "",
+    "logging_dir" : ""
 }
